614d985a4e768ce7a215f8d276bbe2f049d6e92c
Sakthivel3344 <arivazhagan.sakthivel@divum.in>
2024-08-31 21:07:17 +0530

    changes

---
+++ src/constants/homePage.js
@@ -0,0 +1,8 @@
+import profileImage from "@/assets/images/homePage/profilepic.jpeg";
+
+export const homeSection = {
+  title_1: "HELLO",
+  title_2: "I AM SAKTHIVEL",
+  title_3: "APPLICATION DEVELOPER",
+  profileImg: profileImage,
+};
--- src/containers/home/homePage.js
+++ src/containers/home/homePage.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React from "react";
 import styles from "./homePage.module.scss";
+import { homeSection } from "@/constants/homePage";
+import Image from "next/image";
 
 const HomePage = () => {
   return (
@@ -7,15 +9,21 @@ const HomePage = () => {
       <section className={styles.Home_home_section}>
         <div className={styles.Home_home_section_content}>
           <div className={styles.Home_home_section_content_one}>
-            <p className={styles.title_1}>HELLO</p>
-            <p className={styles.title_1}>I AM SAKTHIVEL</p>
-            <p className={styles.title_2}>APPLICATION DEVELOPER</p>
+            <p className={styles.title_1}>{homeSection.title_1}</p>
+            <p className={styles.title_1}>{homeSection.title_2}</p>
+            <p className={styles.title_2}>{homeSection.title_3}</p>
           </div>
-          <div className={styles.Home_home_section_content_two}></div>
+          <Image
+            className={styles.Home_home_section_content_two}
+            src={homeSection.profileImg}
+            height={"25"}
+            width={"15"}
+            unoptimized={true}
+          />
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
 export default HomePage;
